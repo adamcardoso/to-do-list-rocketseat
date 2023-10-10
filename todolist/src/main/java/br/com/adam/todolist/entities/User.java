3cdf0bb9a7cf1579e24d56fe5c48171a3849710e
Adam Cardoso <adamjuniorcardoso@gmail.com>
2023-10-10 16:54:51 -0300

    refatorando o código e criando UserRepository

--- todolist/src/main/java/br/com/adam/todolist/entities/User.java
+++ todolist/src/main/java/br/com/adam/todolist/entities/User.java
@@ -1,19 +1,32 @@
 package br.com.adam.todolist.entities;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_users")
 public class User {
 
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID uuid;
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
